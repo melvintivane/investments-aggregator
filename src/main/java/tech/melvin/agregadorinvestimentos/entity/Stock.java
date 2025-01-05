dc88cca68000617ec2078ded44f0d4ccdc52119b
Melvin Tivane <zen@localhost.localdomain>
2025-01-05 19:39:11 +0200

    create entities and repositories

---
+++ src/main/java/tech/melvin/agregadorinvestimentos/entity/Stock.java
@@ -0,0 +1,4 @@
+package tech.melvin.agregadorinvestimentos.entity;
+
+public class Stock {
+}
---
+++ src/main/java/tech/melvin/agregadorinvestimentos/repository/StockRepository.java
@@ -0,0 +1,4 @@
+package tech.melvin.agregadorinvestimentos.repository;
+
+public interface StockRepository {
+}
