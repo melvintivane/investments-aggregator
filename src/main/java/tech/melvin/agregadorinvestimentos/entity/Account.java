11fcdcd4553a1e8450244fb8d80f695128a2dd0b
Melvin Tivane <zen@localhost.localdomain>
2025-01-06 10:44:39 +0200

    feat(entity): update Account entity with relationships
    - Added a many-to-one relationship with the  entity.
    - Added a one-to-one relationship with the  entity using  and .
    - Introduced a one-to-many relationship with the  entity.
    - Annotated fields for database mapping with JPA annotations.
    - Included constructors, getters, and setters for all fields.

--- src/main/java/tech/melvin/agregadorinvestimentos/entity/Account.java
+++ src/main/java/tech/melvin/agregadorinvestimentos/entity/Account.java
@@ -1,4 +1,64 @@
 package tech.melvin.agregadorinvestimentos.entity;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_accounts")
 public class Account {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.UUID)
+   @Column(name = "account_id")
+   private UUID accountId;
+
+   @ManyToOne
+   @JoinColumn(name = "user_id")
+   private User user;
+
+   @OneToOne(cascade = CascadeType.ALL, mappedBy = "account")
+   @PrimaryKeyJoinColumn
+   private BillingAddress billingAddress;
+
+   @Column(name = "description")
+   private String description;
+
+   @OneToMany(mappedBy = "account")
+   private List<AccountStock> accountStocks = new ArrayList<>();
+
+   public Account() {
+   }
+
+   public Account(UUID accountId, String description, List<AccountStock> accountStocks) {
+      this.accountId = accountId;
+      this.description = description;
+      this.accountStocks = accountStocks;
+   }
+
+   public UUID getAccountId() {
+      return accountId;
+   }
+
+   public void setAccountId(UUID accountId) {
+      this.accountId = accountId;
+   }
+
+   public User getUser() {
+      return user;
+   }
+
+   public void setUser(User user) {
+      this.user = user;
+   }
+
+   public String getDescription() {
+      return description;
+   }
+
+   public void setDescription(String description) {
+      this.description = description;
+   }
 }
