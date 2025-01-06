c178e463c9ba949f3fa44af90dcc4106c33ba589
Melvin Tivane <zen@localhost.localdomain>
2025-01-06 10:43:21 +0200

    feat(entity): add BillingAddress entity
    - Created  entity with fields , , and .
    - Added a one-to-one relationship with the  entity using  and .
    - Annotated fields with JPA annotations for proper database mapping.
    - Included constructors, getters, and setters for all fields.

--- src/main/java/tech/melvin/agregadorinvestimentos/entity/BillingAddress.java
+++ src/main/java/tech/melvin/agregadorinvestimentos/entity/BillingAddress.java
@@ -1,4 +1,58 @@
 package tech.melvin.agregadorinvestimentos.entity;
 
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_billing_address")
 public class BillingAddress {
+
+   @Id
+   @Column(name = "account_id")
+   private UUID accountId;
+
+   @OneToOne(cascade = CascadeType.ALL)
+   @MapsId
+   @JoinColumn(name = "account_id")
+   private Account account;
+
+   @Column(name = "street")
+   private String street;
+
+   @Column(name = "number")
+   private Integer number;
+
+   public BillingAddress() {
+   }
+
+   public BillingAddress(UUID accountId, String street, Integer number) {
+      this.accountId = accountId;
+      this.street = street;
+      this.number = number;
+   }
+
+   public UUID getId() {
+      return accountId;
+   }
+
+   public void setId(UUID id) {
+      this.accountId = id;
+   }
+
+   public String getStreet() {
+      return street;
+   }
+
+   public void setStreet(String street) {
+      this.street = street;
+   }
+
+   public Integer getNumber() {
+      return number;
+   }
+
+   public void setNumber(Integer number) {
+      this.number = number;
+   }
 }
