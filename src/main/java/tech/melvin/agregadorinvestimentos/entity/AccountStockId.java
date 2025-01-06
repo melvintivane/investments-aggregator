27edd8ae6e7b5c4a7e1e47cc147ebec7ed9a3af4
Melvin Tivane <zen@localhost.localdomain>
2025-01-06 11:17:36 +0200

    refactor(entity): update AccountStockId to use UUID for stockId and implement equals/hashCode
    - Changed the  field from  to  for consistency and improved type safety.
    - Added  and  methods using  to ensure proper behavior in collections and comparisons.
    - Adjusted constructors, getters, and setters to accommodate the new  type for .

--- src/main/java/tech/melvin/agregadorinvestimentos/entity/AccountStockId.java
+++ src/main/java/tech/melvin/agregadorinvestimentos/entity/AccountStockId.java
@@ -3,6 +3,7 @@ package tech.melvin.agregadorinvestimentos.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 
+import java.util.Objects;
 import java.util.UUID;
 
 @Embeddable
@@ -12,12 +13,12 @@ public class AccountStockId {
    private UUID accountId;
 
    @Column(name = "stock_id")
-   private String stockId;
+   private UUID stockId;
 
    public AccountStockId() {
    }
 
-   public AccountStockId(UUID accountId, String stockId) {
+   public AccountStockId(UUID accountId, UUID stockId) {
       this.accountId = accountId;
       this.stockId = stockId;
    }
@@ -30,11 +31,24 @@ public class AccountStockId {
       this.accountId = accountId;
    }
 
-   public String getStockId(String stockId) {
+   public UUID getStockId() {
       return stockId;
    }
 
-   public void setStockId(String stockId) {
+   public void setStockId(UUID stockId) {
       this.stockId = stockId;
+   }
+
+   @Override
+   public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      AccountStockId that = (AccountStockId) o;
+      return Objects.equals(accountId, that.accountId) && Objects.equals(stockId, that.stockId);
+   }
+
+   @Override
+   public int hashCode() {
+      return Objects.hash(accountId, stockId);
    }
 }
