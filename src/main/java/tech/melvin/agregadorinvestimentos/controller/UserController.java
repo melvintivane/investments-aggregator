e4fae92b7fea47eded5e1695bffcc27fa557fdc1
Melvin Tivane <zen@localhost.localdomain>
2025-01-08 14:26:16 +0200

    feat(controller): add endpoints for account management

--- src/main/java/tech/melvin/agregadorinvestimentos/controller/UserController.java
+++ src/main/java/tech/melvin/agregadorinvestimentos/controller/UserController.java
@@ -2,9 +2,8 @@ package tech.melvin.agregadorinvestimentos.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import tech.melvin.agregadorinvestimentos.dto.UserCreateDTO;
-import tech.melvin.agregadorinvestimentos.dto.UserResponseDTO;
-import tech.melvin.agregadorinvestimentos.dto.UserUpdateDTO;
+import tech.melvin.agregadorinvestimentos.dto.*;
+import tech.melvin.agregadorinvestimentos.entity.Account;
 import tech.melvin.agregadorinvestimentos.entity.User;
 import tech.melvin.agregadorinvestimentos.service.UserService;
 
@@ -35,25 +34,42 @@ public class UserController {
         return ResponseEntity.ok().body(users);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Optional<UserResponseDTO>> getUser(@PathVariable("id") String userId) {
+    @GetMapping("/{userId}")
+    public ResponseEntity<Optional<UserResponseDTO>> getUser(@PathVariable("userId") String userId) {
         Optional<UserResponseDTO> user = userService.getUserById(userId);
 
         return ResponseEntity.ok().body(user);
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Void> updateUser(@PathVariable("id") String id, @RequestBody UserUpdateDTO userUpdateDTO) {
-        userService.updateUser(id, userUpdateDTO);
+    @PutMapping("/{userId}")
+    public ResponseEntity<Void> updateUser(@PathVariable("userId") String userId, @RequestBody UserUpdateDTO userUpdateDTO) {
+        userService.updateUser(userId, userUpdateDTO);
 
         return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteUser(@PathVariable("id") String userId) {
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Void> deleteUser(@PathVariable("userId") String userId) {
         userService.deleteById(userId);
 
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/{userId}/accounts")
+    public ResponseEntity<Void> createAccount(@PathVariable("userId") String userId, @RequestBody CreateAccountDTO accountDTO) {
+        userService.createAccount(userId, accountDTO);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{userId}/accounts")
+    public ResponseEntity<List<AccountResponseDTO>> getUsersAccounts(@PathVariable("userId") String userId) {
+        var accounts = userService.findAccounts(userId)
+             .stream()
+             .map(account -> new AccountResponseDTO(account.getAccountId().toString(), account.getDescription()))
+             .toList();
+
+        return ResponseEntity.ok().body(accounts);
     }
 
 }
