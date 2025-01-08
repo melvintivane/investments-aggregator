6e5bebf01257b8b7a9903669247db990848ba214
Melvin Tivane <zen@localhost.localdomain>
2025-01-08 16:38:39 +0200

    feat(service): add stock creation functionality
    - Implemented  method to create and save a new stock entity from a .
    - Added dependency injection for  to handle stock persistence.
    
    These changes enhance the service by enabling stock creation within the system.

--- src/main/java/tech/melvin/agregadorinvestimentos/service/StockService.java
+++ src/main/java/tech/melvin/agregadorinvestimentos/service/StockService.java
@@ -1,4 +1,24 @@
 package tech.melvin.agregadorinvestimentos.service;
 
+import org.springframework.stereotype.Service;
+import tech.melvin.agregadorinvestimentos.dto.CreateStockDTO;
+import tech.melvin.agregadorinvestimentos.entity.Stock;
+import tech.melvin.agregadorinvestimentos.repository.StockRepository;
+
+@Service
 public class StockService {
+   private final StockRepository stockRepository;
+
+   public StockService(StockRepository stockRepository) {
+      this.stockRepository = stockRepository;
+   }
+
+   public Stock createStock(CreateStockDTO stockDTO) {
+      var stock = new Stock(
+           stockDTO.stockId(),
+           stockDTO.description()
+      );
+
+      return stockRepository.save(stock);
+   }
 }
