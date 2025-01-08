408974ce06d4a35fe8cacbcb7949cf57dbe8d5d8
Melvin Tivane <zen@localhost.localdomain>
2025-01-08 14:21:42 +0200

    chore(repository): add @Repository annotation to StockRepository

--- src/main/java/tech/melvin/agregadorinvestimentos/repository/StockRepository.java
+++ src/main/java/tech/melvin/agregadorinvestimentos/repository/StockRepository.java
@@ -1,9 +1,11 @@
 package tech.melvin.agregadorinvestimentos.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import tech.melvin.agregadorinvestimentos.entity.Stock;
 
 import java.util.UUID;
 
+@Repository
 public interface StockRepository extends JpaRepository<Stock, UUID> {
 }
