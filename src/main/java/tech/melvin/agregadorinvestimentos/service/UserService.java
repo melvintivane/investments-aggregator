f68ce5246ee01f1a4ba8237b37aedfb5308853d1
Melvin Tivane <zen@localhost.localdomain>
2025-01-08 14:24:59 +0200

    feat(service): add account creation and retrieval functionality

--- src/main/java/tech/melvin/agregadorinvestimentos/service/UserService.java
+++ src/main/java/tech/melvin/agregadorinvestimentos/service/UserService.java
@@ -3,22 +3,32 @@ package tech.melvin.agregadorinvestimentos.service;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+import tech.melvin.agregadorinvestimentos.dto.CreateAccountDTO;
 import tech.melvin.agregadorinvestimentos.dto.UserCreateDTO;
 import tech.melvin.agregadorinvestimentos.dto.UserResponseDTO;
 import tech.melvin.agregadorinvestimentos.dto.UserUpdateDTO;
+import tech.melvin.agregadorinvestimentos.entity.Account;
+import tech.melvin.agregadorinvestimentos.entity.BillingAddress;
 import tech.melvin.agregadorinvestimentos.entity.User;
+import tech.melvin.agregadorinvestimentos.repository.AccountRepository;
+import tech.melvin.agregadorinvestimentos.repository.BillingAddressRepository;
 import tech.melvin.agregadorinvestimentos.repository.UserRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class UserService {
+    private final AccountRepository accountRepository;
+    private final BillingAddressRepository billingAddressRepository;
     private final UserRepository userRepository;
 
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserService(AccountRepository accountRepository, BillingAddressRepository billingAddressRepository, UserRepository userRepository) {
+       this.accountRepository = accountRepository;
+       this.billingAddressRepository = billingAddressRepository;
+       this.userRepository = userRepository;
     }
 
     public User createUser(UserCreateDTO userCreateDTO) {
@@ -102,6 +112,42 @@ public class UserService {
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found.");
         }
+    }
+
+    public void createAccount(String userId, CreateAccountDTO accountDTO) {
+
+        var user = userRepository.findById(UUID.fromString(userId))
+             .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found."));
+
+        if (user.getAccounts() == null) {
+            user.setAccounts(new ArrayList<>());
+        }
+
+        // DTO -> ENTITY
+        var account = new Account(
+             user,
+             null,
+             accountDTO.description(),
+             new ArrayList<>()
+        );
+
+        var accountCreated = accountRepository.save(account);
+
+        var billingAddress = new BillingAddress(
+             accountCreated.getAccountId(),
+             accountCreated,
+             accountDTO.street(),
+             accountDTO.number()
+        );
+
+        billingAddressRepository.save(billingAddress);
+    }
+
+    public List<Account> findAccounts(String userId) {
+        var user = userRepository.findById(UUID.fromString(userId))
+             .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found."));
+
+        return user.getAccounts();
     }
 
     private boolean isValidEmail(String email) {
