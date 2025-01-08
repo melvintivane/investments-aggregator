3fb0793d7778610af7f4ccb87aa375d9d1802833
Melvin Tivane <zen@localhost.localdomain>
2025-01-08 14:21:09 +0200

    chore(repository): add @Repository annotation to BillingAddressRepository

--- src/main/java/tech/melvin/agregadorinvestimentos/repository/BillingAddressRepository.java
+++ src/main/java/tech/melvin/agregadorinvestimentos/repository/BillingAddressRepository.java
@@ -1,9 +1,11 @@
 package tech.melvin.agregadorinvestimentos.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import tech.melvin.agregadorinvestimentos.entity.BillingAddress;
 
 import java.util.UUID;
 
+@Repository
 public interface BillingAddressRepository extends JpaRepository<BillingAddress, UUID> {
 }
