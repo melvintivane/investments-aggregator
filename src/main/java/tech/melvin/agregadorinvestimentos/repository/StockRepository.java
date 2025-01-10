28d00e8fabdc2ffd98688c39d87988f895b158e3
Melvin Tivane <zen@localhost.localdomain>
2025-01-10 11:58:33 +0200

    refactor: change UUID type to String

--- src/main/java/tech/melvin/agregadorinvestimentos/repository/StockRepository.java
+++ src/main/java/tech/melvin/agregadorinvestimentos/repository/StockRepository.java
@@ -7,5 +7,5 @@ import tech.melvin.agregadorinvestimentos.entity.Stock;
 import java.util.UUID;
 
 @Repository
-public interface StockRepository extends JpaRepository<Stock, UUID> {
+public interface StockRepository extends JpaRepository<Stock, String> {
 }
