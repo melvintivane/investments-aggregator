bc7e3a6877e1d4c5c05d2ec8af9d406bc8784a7b
Melvin Tivane <zen@localhost.localdomain>
2025-01-10 11:53:31 +0200

    feat(account-service): integrate BrapiClient for stock price calculation
    - Added BrapiClient as a dependency to fetch stock prices.
    - Injected TOKEN from environment variables using @Value annotation.
    - Enhanced  to include the total value of stocks by multiplying the quantity with the market price.
    - Implemented  method to calculate total stock value using external API.
    
    This update allows the service to calculate real-time stock values, improving data accuracy for account stocks.

--- src/main/java/tech/melvin/agregadorinvestimentos/service/AccountService.java
+++ src/main/java/tech/melvin/agregadorinvestimentos/service/AccountService.java
@@ -1,8 +1,10 @@
 package tech.melvin.agregadorinvestimentos.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+import tech.melvin.agregadorinvestimentos.client.BrapiClient;
 import tech.melvin.agregadorinvestimentos.dto.AccountStockDTO;
 import tech.melvin.agregadorinvestimentos.dto.AccountStockResponseDTO;
 import tech.melvin.agregadorinvestimentos.entity.AccountStock;
@@ -17,14 +19,18 @@ import java.util.UUID;
 @Service
 public class AccountService {
 
+   @Value("#{environment.TOKEN}")
+   private String TOKEN;
    private final AccountRepository accountRepository;
    private final AccountStockRepository accountStockRepository;
    private final StockRepository stockRepository;
+   private final BrapiClient brapiClient;
 
-   public AccountService(AccountRepository accountRepository, AccountStockRepository accountStockRepository, StockRepository stockRepository) {
+   public AccountService(AccountRepository accountRepository, AccountStockRepository accountStockRepository, StockRepository stockRepository, BrapiClient brapiClient) {
       this.accountRepository = accountRepository;
       this.accountStockRepository = accountStockRepository;
       this.stockRepository = stockRepository;
+      this.brapiClient = brapiClient;
    }
 
    public AccountStock associateStock(String accountId, AccountStockDTO accountStockDTO) {
@@ -49,7 +55,18 @@ public class AccountService {
 
       return account.getAccountStocks()
            .stream()
-           .map(ac -> new AccountStockResponseDTO(ac.getStock().getStockId(), ac.getQuantity(), 0.0))
+           .map(ac -> new AccountStockResponseDTO(
+                ac.getStock().getStockId(),
+                ac.getQuantity(),
+                getTotal(ac.getQuantity(), ac.getStock().getStockId())))
            .toList();
+   }
+
+   private double getTotal(Integer quantity, String stockId) {
+      var response = brapiClient.getQuote(TOKEN, stockId);
+
+      var price = response.results().getFirst().regularMarketPrice();
+
+      return quantity * price;
    }
 }
