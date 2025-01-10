8e1a50df85dbd40ab48bef422da2bc25176857d6
Melvin Tivane <zen@localhost.localdomain>
2025-01-10 14:27:13 +0200

    feat(app): enable Feign clients and minor improvements in UserController
    - Annotated  with  to enable Feign client functionality.
    - Added minor formatting improvements in .
    
    These updates ensure Feign clients are properly configured for API communication and improve code readability.

--- src/main/java/tech/melvin/agregadorinvestimentos/controller/UserController.java
+++ src/main/java/tech/melvin/agregadorinvestimentos/controller/UserController.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
     private final UserService userService;
 
     public UserController(UserService userService) {
